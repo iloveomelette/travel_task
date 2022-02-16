ee986a7f12b8e58b44aaa5b57c8120dc733f4943
iloveomelette <mizuki1998hosei@gmail.com>
2022-02-16 21:30:03 +0900

    プランの定義とプランを尋ねるメッセージを追加

---
+++ travel_task_method/main.rb
@@ -0,0 +1,12 @@
+# ファイルの参照
+require "./methods"
+
+# 旅行プランの定義
+  plans = [
+    { place: "沖縄", price: 10000 },
+    { place: "北海道", price: 20000 },
+    { place: "九州", price: 15000 }
+  ]
+
+# 旅行プランを尋ねるメッセージ
+ask_plan
---
+++ travel_task_method/methods.rb
@@ -0,0 +1,9 @@
+# 旅行プランを尋ねるメッセージ
+def ask_plan
+  puts <<~TEXT
+  
+  旅行プランを選択して下さい。
+  
+  TEXT
+end
+
